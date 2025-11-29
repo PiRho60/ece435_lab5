456b5470a193554c4c5ede3bf1694c3a83da428e
Rohan Patra <mohantyran@gmail.com>
2025-11-28 21:45:48 -0500

    python file added

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,19 @@
-def read_csv()
+from typing import Any, Tuple
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+
+def read_csv(file: str) -> tuple[Any, Any]:
+    """Read CSV file."""
+    x_data, fft_data = np.loadtxt(file, delimiter=',', skiprows=2, unpack=True)
+    return x_data, fft_data
+
+
+def plot_data(file: str):
+    """Plot data from CSV file."""
+    x_data, fft_data = read_csv(file)
+    plt.plot(x_data, fft_data)
+    plt.show()
+
+if __name__ == '__main__':
